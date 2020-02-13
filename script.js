39d56d6f3c2ef47a5fdd2c6af6137781214a4d8b
chayunNazariy <57292782+chayunNazariy@users.noreply.github.com>
2020-02-13 13:27:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 var isAdmin = confirm('Вы - администратор?')
 var pas = '123321'
 if (isAdmin) {
-          var userPassword = prompt('Введите пароль. (осталось 3 попытки)')
-          if (pas===userPassword) {
-              console.log('Добро пожаловать, администратор')
-          }
-    else if (pas!==userPassword) {
-        var userPassword = prompt('Введите пароль. (осталось 2 попытки)')
-        }
-   var userPassword = prompt('Введите пароль. (осталaсь 1 попыткa)')
-   if (pas===userPassword){
-           console.log('Добро пожаловать, администратор')    
-   }
     
-    else {
-            console.log('bAN на 3 дня')    
+    var userPassword = prompt('Введите пароль. (осталось 2 попытки)')
+    
+    if (pas == userPassword) {
+        
+        console.log('Добро пожаловать, администратор')
+    }
+        
+        
+        
+     else if (pas !== userPassword) {
+        
+        var userPassword = prompt('Введите пароль. (осталось 1 попытки)')
+          if (pas !== userPassword) { 
+        console.log('bAN на 3 дня')
+          }
+        else {
+            console.log('Добро пожаловать, администратор')
+        }
     }
 }
+    
 
-else {
+
+    
+    
+    
+    
+ else {
+    
     console.log('Добро пожаловать, гость')
-}                       
-                      
+}
